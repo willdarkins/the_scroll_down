5e22f9e900c94b27dacc0f13a1395a3083d412ed
Will Darkins <willdarkins@gmail.com>
2022-01-09 22:53:39 -0800

    added some initial style components to the Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
+import logo from '../images/newspaper_1f4f0.png'
 
 const NavBarStyles = styled.div `
-
+    background-color: var(--nav-bar);
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem calc((100vw - 1000px) / 2);
+    z-index: 10;
 `
 
 function Navbar() {
     return (
-        <>
-        <h1>Navbar</h1>
-        </>
+        <NavBarStyles>
+                <img src={logo} alt="Logo" />
+        </NavBarStyles>
     )
 }
 
