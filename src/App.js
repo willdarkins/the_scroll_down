33a00144fb07d9b51728be0ba2cbd114f96a4978
Will Darkins <willdarkins@gmail.com>
2022-01-09 22:08:54 -0800

    added dependencies for styling - created styles subdirectory and made a GlobalStyles sheet from styled-components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const AppStyles = styled.div `
+  background-color: red;
+`
+
+function App() {
+  return (
+    <AppStyles>
+      <h1>Hello World!</h1>
+    </AppStyles>
+  )
+}
+
+export default App
---
+++ styles/GlobalStyles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle, createGlobalStyle } from "styled-components";
+
+const createGlobalStyle = createGlobalStyle`
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+`
