34ad4cefa3d685ef63465a951f89b8a11cbd1a6e
Will Darkins <willdarkins@gmail.com>
2022-01-09 23:13:40 -0800

    addead a few more styles to the navbar and put the logo image and text in the same div. now going to make search bar from material ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,36 @@ import React from 'react'
 import styled from 'styled-components'
 import logo from '../images/newspaper_1f4f0.png'
 
-const NavBarStyles = styled.div `
+const NavBarStyles = styled.div`
     background-color: var(--nav-bar);
     height: 80px;
     display: flex;
     justify-content: space-between;
     padding: 0.5rem calc((100vw - 1000px) / 2);
     z-index: 10;
+    .logo{
+        display: flex;
+        img{
+        cursor: pointer; 
+        }
+        h1{
+        margin-top: .9rem;
+        color: #fff;
+        text-decoration: none;
+        padding: 0 1rem;
+        cursor: pointer;
+        }
+    }
+
 `
 
 function Navbar() {
     return (
         <NavBarStyles>
+            <div className='logo'>
                 <img src={logo} alt="Logo" />
+                <h1>The Scroll Down</h1>
+            </div>
         </NavBarStyles>
     )
 }
