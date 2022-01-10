3f121671fc8a6c4058adb252d80d4541dba62ad4
Will Darkins <willdarkins@gmail.com>
2022-01-09 22:13:56 -0800

    added GlobalStyles to index.js

--- styles/GlobalStyles.js
+++ styles/GlobalStyles.js
@@ -1,9 +1,14 @@
 import { createGlobalStyle, createGlobalStyle } from "styled-components";
 
-const createGlobalStyle = createGlobalStyle`
+const GlobalStyles = createGlobalStyle`
     *{
         padding: 0;
         margin: 0;
         box-sizing: border-box;
     }
+    body{
+        background-color: #F5F5F5;
+    }
 `
+
+export default GlobalStyles
