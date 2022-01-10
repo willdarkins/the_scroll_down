7e3ebb3173dfb64d5d547729f3132f75fd45cf28
Will Darkins <willdarkins@gmail.com>
2022-01-09 22:26:50 -0800

    added a few styles to the global - including variable colors for gloabl and navbar

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,13 +1,20 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+    :root{
+        --background: #F5F5F5;
+        --nav-bar: #002838;
+    }
+
     *{
         padding: 0;
         margin: 0;
         box-sizing: border-box;
+        list-style: none;
+        text-decoration: none;
     }
     body{
-        background-color: #F5F5F5;
+        background-color: var(--background)
     }
 `
 
