4df4306eaa80e8dd0ea8b04f6ad3fa8b93f227c8
Will Darkins <willdarkins@gmail.com>
2022-01-11 01:29:02 -0800

    sign up sheet has been created

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import { Grid, Paper, Typography } from '@material-ui/core'
+import styled from 'styled-components'
+import { Grid, Paper, Typography, TextField } from '@material-ui/core'
+import Button from '@mui/material/Button';
 import signupicon from '../images/signup_icon.png'
 
+const FormStyles = styled.div`
+    .formstyles{
+        margin: .5rem auto;
+    }
+    .btn{
+        margin-top: 2rem;
+    }
+`
+    
+
+
 function SignUp() {
-    const paperStyle = { padding: '30px 20px', width: 500, margin: '20px, auto' }
+    const paperStyle = { padding: '30px 20px', width: 400, margin: '4.5rem auto' }
     const headerStyle = { margin: '0' }
     return (
         <Grid>
@@ -13,6 +26,67 @@ function SignUp() {
                     <h2 style={headerStyle}>Sign Up</h2>
                     <Typography variant='caption'>Fill out the form, become a member and start saving news you care about</Typography>
                 </Grid>
+                <form className='formstyles'>
+
+                <Grid container spacing={2}>
+                            <Grid xs={12} sm={6} item>
+                                <TextField
+                                    type='input'
+                                    label='First Name'
+                                    placeholder='First Name'
+                                    variant='outlined'
+                                    fullWidth
+                                    required/>
+                            </Grid>
+                            <Grid xs={12} sm={6} item>
+                            <TextField
+                                    type='input'
+                                    label='Last Name'
+                                    placeholder='Last Name'
+                                    variant='outlined'
+                                    fullWidth
+                                    required/>
+                            </Grid>
+                            <Grid xs={12} item>
+                            <TextField
+                                    type='input'
+                                    label='Email'
+                                    placeholder='Email'
+                                    variant='outlined'
+                                    fullWidth
+                                    required/>
+                            </Grid>
+                            <Grid xs={12} item>
+                            <TextField
+                                    type='input'
+                                    label='Password'
+                                    placeholder='Password'
+                                    variant='outlined'
+                                    fullWidth
+                                    required/>
+                            </Grid>
+                            <Grid xs={12} item>
+                                <Button
+                                    type='submit' color='primary' variant='contained' fullWidth>Sign Up!</Button>
+                            </Grid>
+                        </Grid>
+
+
+
+                    {/* <Grid item spacing={5}>
+                    <TextField fullWidth label ='First Name' placeholder='Enter your first name'/>
+                    </Grid>
+                    <Grid item spacing={3}>
+                    <TextField fullWidth label ='Last Name' placeholder='Enter your last name'/>
+                    </Grid>
+                    <Grid item spacing={3}>
+                    <TextField fullWidth label ='Email' placeholder='Enter your email address'/>
+                    </Grid>
+                    <Grid item spacing={3}>
+                    <TextField fullWidth label ='Password' placeholder='Enter a password'/>
+                    </Grid>
+                    <Button className='btn' type='submit' variant='contained' color='primary'>Sign Up!</Button> */}
+                </form>
             </Paper>
         </Grid>
     )
