7b81548b10687005a5d21d8cd714bff10dd86c5a
Will Darkins <willdarkins@gmail.com>
2022-01-10 21:23:13 -0800

    added new styles to menu bar and trying to get things aligned

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,94 @@
 import React from 'react'
 import styled from 'styled-components'
 import logo from '../images/newspaper_1f4f0.png'
-import Search from './Search'
+import TravelExploreIcon from '@mui/icons-material/TravelExplore';
+import { ButtonBase } from '@mui/material';
+
 
 const NavBarStyles = styled.div`
-    background-color: var(--nav-bar);
-    height: 80px;
-    display: flex;
-    justify-content: space-between;
-    padding: 0.5rem calc((100vw - 1000px) / 2);
-    z-index: 10;
-    .logo{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    .nav{
         display: flex;
-        img{
-        cursor: pointer; 
-        }
-        h1{
-        margin-top: .9rem;
-        color: #fff;
+        width: 100%;
+        background-color: var(--nav-bar);
+        position: relative;
+        justify-content: space-between;
+        text-align: center;
+        padding: 15px 30px;
+    }
+    button{
+        background-color: #4CAF50;
+        border: none;
+        border-radius: 20px;
+        color: white;
+        padding: 10px 25px;
+        text-align: center;
         text-decoration: none;
-        padding: 0 1rem;
+        display: inline-block;
+        font-size: 16px;
+        margin-left: .3rem;
+        /* margin: 4px 2px; */
         cursor: pointer;
+        transition-duration: 0.4s;
+        button:hover{
+            box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
         }
+    }
+    .nav ol{
+        display: flex;
+        margin: auto 0;
+    }
+    .nav ol li{
+        margin: 0 2px;
+        font-size: 20px;
+        letter-spacing: 1px;
+        padding: 5px 10px;
+        color: white;
+        cursor: pointer;
+    }
+    .nav ol li:hover{
+        background: white;
+        color: var(--nav-bar);
+    }
+    .nav .search_box{
+        display: flex;
+        margin: auto 0;
+        height: 35px;
+        line-height: 35px;
+
+    }
+    .nav .search_box input{
+        border: none;
+        outline: none;
+        background: white;
+        height: 100%;
+        padding: 0 10px;
+        font-size: 20px;
+        width: 350px;
+        border-radius: 20px;
     }
 
 `
-
 function Navbar() {
     return (
         <NavBarStyles>
-            <div className='logo'>
-                <img src={logo} alt="Logo" />
-                <h1>The Scroll Down</h1>
+            <div className='nav'>
+                {/* <img src={logo} />  */}
+                <div className='icon'>The Scroll Down</div>
+                <div className='search_box'>
+                    <input type={"serach"} placeholder='Search for News' />
+                    <button>Serach</button>
+                </div>
+                <ol>
+                    <li>Home</li>
+                    <li>Sign In</li>
+                    <li>Sign Up</li>
+                    <li>About</li>
+                    <li>Saved News</li>
+                </ol>
             </div>
-            <Search />
         </NavBarStyles>
     )
 }
