67a9374fbfaab4a6f0f690a651bced155aedad24
Will Darkins <willdarkins@gmail.com>
2022-01-10 23:36:10 -0800

    made a few other pages for sign in... sign up and saved stories

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import styled from 'styled-components'
 import Navbar from './components/Navbar'
 import Homepage from './pages/Homepage'
 
-const AppStyles = styled.div `
+const AppStyles = styled.div`
 `
 
 function App() {
   return (
-    <AppStyles>
-      <Navbar />
-      <Homepage />
-    </AppStyles>
+    <>
+    <Router>
+    <Navbar />
+      <AppStyles>
+        
+        <Homepage />
+      </AppStyles>
+      </Router>
+    </>
   )
 }
 
---
+++ src/pages/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignUp() {
+    return (
+        <>
+            <h1>SIGN UP</h1>
+        </>
+    )
+}
+
+export default SignUp
