d6a508c9cb4f8c2c17a7000153b7ed529ca405bf
Will Darkins <willdarkins@gmail.com>
2022-01-11 22:27:33 -0800

    created news-grid styles for cards that will lay 3-by-1

--- src/pages/SavedNews.js
+++ src/pages/SavedNews.js
@@ -2,11 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 const SavedStyles = styled.div`
-    .news-grid{
+    .news-page{
         padding: 30px 170px;
         .search-grid{
             letter-spacing: -1px;
             font-size: 40px;
+        }
+        .news-grid{
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            grid-gap: 50px;
+            padding: 40px 0;
         }
     }
 
@@ -15,8 +21,11 @@ const SavedStyles = styled.div`
 function SavedNews() {
     return (
         <>
-        <div className='news-grid'>
+        <div className='news-page'>
         <h1 className='search-header'>Search Term Will Go Here</h1>
+        <div className='news-grid'>
+
+        </div>
         </div>
         </>
     )
