35fc1ae42b37a0fd0f43c8cdee71d08653c1dba9
Will Darkins <willdarkins@gmail.com>
2022-01-11 19:46:26 -0800

    added Grid, Paper and some inline styling to SignIn.js

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Grid, Paper } from '@material-ui/core'
 
 function SignIn() {
+  const paperStyles ={padding: 20, width: 460,}
     return (
         <>
-          <h1>SIGN IN</h1>  
+          <Grid>
+            <Paper elevation={10} style={paperStyles}>
+            Sign In
+            </Paper>
+          </Grid>
         </>
     )
 }
