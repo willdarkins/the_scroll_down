012344b88a0a68a1f92328536159730b11ce1017
Will Darkins <willdarkins@gmail.com>
2022-01-11 22:40:13 -0800

    added news card styles with grid layout

--- src/pages/SavedNews.js
+++ src/pages/SavedNews.js
@@ -2,32 +2,40 @@ import React from 'react'
 import styled from 'styled-components'
 
 const SavedStyles = styled.div`
-    .news-page{
-        padding: 30px 170px;
-        .search-grid{
-            letter-spacing: -1px;
-            font-size: 40px;
-        }
-        .news-grid{
-            display: grid;
-            grid-template-columns: repeat(3, 1fr);
-            grid-gap: 50px;
-            padding: 40px 0;
+    padding: 30px 170px;
+    .search-header{
+        letter-spacing: -1px;
+        font-size: 40px;
+    }
+    .news-grid{
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 50px;
+        padding: 40px 0;
+        .news-card{
+            min-width: 300px;
+            margin: auto;
+            background: white;
+            box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
+            height: 100%;
+            text-decoration: none;
+            color: var(--font-dark);
         }
     }
+
 
 `
 
 function SavedNews() {
     return (
-        <>
-        <div className='news-page'>
-        <h1 className='search-header'>Search Term Will Go Here</h1>
-        <div className='news-grid'>
+        <SavedStyles>
+            <h1 className='search-header'>Search Term Will Go Here</h1>
+            <div className='news-grid'>
+                <div className='news-card'>
 
-        </div>
-        </div>
-        </>
+                </div>
+            </div>
+        </SavedStyles>
     )
 }
 
