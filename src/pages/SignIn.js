d36a2fea393c54246d1ed8a0b55bf6303bc249ad
Will Darkins <willdarkins@gmail.com>
2022-01-11 19:47:25 -0800

    added some margin to card... looking centered now..

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Grid, Paper } from '@material-ui/core'
 
 function SignIn() {
-  const paperStyles ={padding: 20, width: 460,}
+  const paperStyles ={padding: 20, width: 460, margin: '4.5rem auto'}
     return (
         <>
           <Grid>
