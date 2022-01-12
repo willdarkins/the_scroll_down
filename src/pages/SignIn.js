e20e7d621c871086d6cb307643284af02dc9345f
Will Darkins <willdarkins@gmail.com>
2022-01-11 19:52:04 -0800

    added header style to card

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Grid, Paper } from '@material-ui/core'
+import signinicon from '../images/log-in.png'
 
 function SignIn() {
   const paperStyles ={padding: 20, width: 460, margin: '4.5rem auto'}
+  const headerStyle = { margin: '0 1rem' }
     return (
         <>
           <Grid>
             <Paper elevation={10} style={paperStyles}>
-            Sign In
+              <img src={signinicon} alt='Sign In icon' />
+            <h2 style={headerStyle}></h2>Sign In
             </Paper>
           </Grid>
         </>
