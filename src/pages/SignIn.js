3f81cd199c4324ff90ce16f1cccba10738373fe4
Will Darkins <willdarkins@gmail.com>
2022-01-11 19:58:27 -0800

    centered everything in the header... looks better now.

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -3,19 +3,21 @@ import { Grid, Paper, Typography } from '@material-ui/core'
 import signinicon from '../images/log-in.png'
 
 function SignIn() {
-  const paperStyles ={padding: 20, width: 460, margin: '4.5rem auto'}
+  const paperStyles = { padding: 20, width: 460, margin: '4.5rem auto' }
   const headerStyle = { margin: '0 1rem' }
-    return (
-        <>
-          <Grid>
-            <Paper elevation={10} style={paperStyles}>
-              <img src={signinicon} alt='Sign In icon' />
+  return (
+    <>
+      <Grid>
+        <Paper elevation={10} style={paperStyles}>
+          <Grid align='center'>
+            <img src={signinicon} alt='Sign In icon' />
             <h2 style={headerStyle}>Sign In</h2>
             <Typography variant='caption'>Provide your credentials, log in, save stories and get informed</Typography>
-            </Paper>
           </Grid>
-        </>
-    )
+        </Paper>
+      </Grid>
+    </>
+  )
 }
 
 export default SignIn
