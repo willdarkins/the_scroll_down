d572930f7741ecd4a666c53b66cec506d271c5c5
Will Darkins <willdarkins@gmail.com>
2022-01-14 23:13:09 -0800

    separtated DarkModeIcon into separate component... hopefully this works!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,101 +1,22 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components'
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import Switch from '@mui/material/Switch';
 import { NavLink, useHistory } from 'react-router-dom'
 import InputBase from '@mui/material/InputBase';
 import { newsStoreContext } from '../utils/store'
+import { useState } from 'react';
+import DarkModeSwitch from './DarkModeSwitch'
 
-import { searchNewsAPI } from '../utils/api'
-import { SAVE_STORY } from '../utils/mutations'
-import { useMutation } from '@apollo/client';
 
 function Navbar() {
-
     // // create state for holding returned news api data
     const [searchedNews, setSearchedNews] = useState('');
     //   // create state for holding our search field data
     const { setNewsInput } = useContext(newsStoreContext)
-
-
-    //   //useMutation() Hook to execute the SAVE_BOOK mutation - this is in place of saveBook() function imported from API file
-    //   const [savestory, { error }] = useMutation(SAVE_STORY);
-
-    //   // create state to hold saved MongoDB values***** NEED TO FIGURE OUT HOW WE WILL USE MONGODB ID WITH CALLBACK FUNCTION
-    //   const [_id, setSavedBookIds] = useState(getSavedBookIds());
-
-    //   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-    //   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
-    //   useEffect(() => {
-    //     return () => saveBookIds(savedBookIds);
-    //   });
-
-    //   // create method to search for books and set state on form submit
-    //   const handleFormSubmit = async (event) => {
-    //     event.preventDefault();
-
-    //     if (!searchInput) {
-    //       return false;
-    //     }
-
-    //     try {
-    //       const response = await searchNewsAPI(searchInput);
-
-    //       if (!response.ok) {
-    //         throw new Error('something went wrong!');
-    //       }
-
-    //       const { items } = await response.json();
-
-    //       const newsData = items.map((news) => ({
-    //         news: news.id,
-    //         source: book.volumeInfo.authors || ['No author to display'],
-    //         title: book.volumeInfo.title,
-    //         description: book.volumeInfo.description,
-    //         link: asdfasdf,
-    //         image: book.volumeInfo.imageLinks?.thumbnail || '',
-    //         publishDate: asdfasdf
-    //       }));
-
-    //       setSearchedBooks(bookData);
-    //       setSearchInput('');
-    //     } catch (err) {
-    //       console.error(err);
-    //     }
-    //   };
-
-    //   // create function to handle saving a book to our database
-    //   const handleSaveBook = async (bookId) => {
-    //     // find the book in `searchedBooks` state by the matching id
-    //     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
-    //     // get token
-    //     const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    //     if (!token) {
-    //       return false;
-    //     }
-
-    //     try {
-    //       await saveBook({variables: bookToSave});
-
-    //       if (!response.ok) {
-    //         throw new Error('something went wrong!');
-    //       }
-
-    //       // if book successfully saves to user's account, save book id to state
-    //       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    //     } catch (err) {
-    //       console.error(err);
-    //     }
-    //   };
-
     const history = useHistory();
 
     const handleOnSubmit = () => {
         setNewsInput(searchedNews)
         history.push(`/searchnews`);
-
     };
     return (
         <NavBarStyles>
@@ -109,17 +30,7 @@ function Navbar() {
                         onChange={(e) => setSearchedNews(e.target.value)}
                     />
                     <button onClick={handleOnSubmit}>Search</button>
-
-                    <div className='light-dark-mode'>
-                        <div className='left-content'>
-                            <DarkModeIcon />
-                        </div>
-                        <div className='right-content'>
-                            <Switch
-                                size='small'
-                            />
-                        </div>
-                    </div>
+                    <DarkModeSwitch />
                 </div>
                 <ol>
                     <li><NavLink exact to='/' activeClassName='current'>Home</NavLink></li>
@@ -200,24 +111,6 @@ const NavBarStyles = styled.div`
         font-size: 20px;
         width: 350px;
         border-radius: 20px;
-    }
-    .light-dark-mode{
-        margin-left: 1rem;
-        margin-top: .3rem;
-        background: #004b69;
-        border-radius: 25%;
-        width: 4.5rem;
-        height: 1.8rem;
-        /* z-index: 15; */
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        svg{
-            display: flex;
-            align-items: center;
-            font-size: 1.3rem;
-            color: white;
-        }
     }
 
 `
