e76e5041355bd6f7154d0fcca8d0d60c9d4d4e11
Will Darkins <willdarkins@gmail.com>
2022-01-15 00:08:54 -0800

    added Dark Mode styling to the news cards for both the saved pages and search page

--- src/pages/SearchNews.js
+++ src/pages/SearchNews.js
@@ -54,7 +54,7 @@ const SavedStyles = styled.div`
             padding: 1rem;
             min-width: 300px;
             margin: auto;
-            background: white;
+            background: var(--dark-card);
             box-shadow: rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset;
             height: 100%;
             text-decoration: none;
